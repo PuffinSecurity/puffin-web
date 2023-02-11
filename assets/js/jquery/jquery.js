b11ec6660c2ded7e5000f8beac03c7c1ab7f1fe6
HamzaWebDev <91342208+Hamzapab@users.noreply.github.com>
2023-02-11 12:53:53 +0100

    Update jquery.js

--- assets/js/jquery/jquery.js
+++ assets/js/jquery/jquery.js
@@ -5,5 +5,11 @@ margin:"",padding:"",border:"Width"},function(a,b){n.cssHooks[a+b]={expand:funct
 ;return X(this,function(b,c,d){var e;return n.isWindow(b)?b.document.documentElement["client"+a]:9===b.nodeType?(e=b.documentElement,Math.max(b.body["scroll"+a],e["scroll"+a],b.body["offset"+a],e["offset"+a],e["client"+a])):void 0===d?n.css(b,c,g):n.style(b,c,d,g)},b,f?d:void 0,f,null)}})}),n.fn.extend({bind:function(a,b,c){return this.on(a,null,b,c)},unbind:function(a,b){return this.off(a,null,b)},delegate:function(a,b,c,d){return this.on(b,a,c,d)},undelegate:function(a,b,c){return 1===arguments.length?this.off(a,"**"):this.off(b,a||"**",c)}}),n.fn.size=function(){return this.length},n.fn.andSelf=n.fn.addBack,"function"==typeof define&&define.amd&&define("jquery",[],function(){return n});var mc=a.jQuery,nc=a.$;return n.noConflict=function(b){return a.$===n&&(a.$=nc),b&&a.jQuery===n&&(a.jQuery=mc),n},b||(a.jQuery=a.$=n),n});
 jQuery.noConflict();
 
+let header = document.querySelector("header");
 
-console.log("im in")
+window.addEventListener("scroll",()=>{
+  if(header.classList.contains("sticky")){
+    console.log("in");
+    
+  }
+})
